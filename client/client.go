10e18d16e8721a96ef99126a7bb88a868c26eba1
ll <duanguge865@gmail.com>
2025-10-18 17:50:40 +0800

    v9.2 新增解析命令行参数以获得ip和端口

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -25,11 +26,23 @@ func NewClient(serverIp string, serverPort string) *Client {
 	client.Conn = conn
 	return client
 }
+
+var serverIP string
+var serverPort string
+
+func init() { //初始化命令行参数
+	flag.StringVar(&serverIP, "ip", "127.0.0.1", "设置服务器IP地址（默认127.0.0.1）")
+	flag.StringVar(&serverPort, "port", "8888", "设置服务器端口号（默认8888）")
+}
 func main() {
-	client := NewClient("127.0.0.1", "8888")
+	//解析命令行参数
+	flag.Parse()
+
+	client := NewClient(serverIP, serverPort)
 	if client == nil {
 		fmt.Println("连接服务器失败")
 		return
 	}
 	fmt.Println("连接服务器成功")
+	select {}
 }
